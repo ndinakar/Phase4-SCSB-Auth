ccb14e23018bb83465e53e7087be9fefc4e8d9f4
kirankumar03 <akula.kirankumar@htcindia.com>
2017-03-07 12:06:24 +0530

    Recap-524 User with Role 'Super Admin' must be able access Roles Tab

--- src/main/java/org/recap/controller/AuthorizationController.java
+++ src/main/java/org/recap/controller/AuthorizationController.java
@@ -3,7 +3,9 @@ package org.recap.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.recap.RecapConstants;
 import org.recap.security.AuthorizationServiceImpl;
 import org.recap.security.UserManagementService;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created by dharmendrag on 10/1/17.
@@ -58,11 +61,26 @@ public class AuthorizationController {
     }
 
     @RequestMapping(value="/userRoles",method= RequestMethod.POST)
-    @ApiOperation(value="reports authentication",notes="Used to Authorizer User for Users & Roles",position = 0,consumes = "application/json")
+    @ApiOperation(value="user authentication",notes="Used to Authorizer User for Users",position = 0,consumes = "application/json")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "User & Role authentication success")})
     public boolean userRoles(@RequestBody UsernamePasswordToken usernamePasswordToken) {
         return authorizationService.checkPrivilege(usernamePasswordToken,userManagementService.getPermissionId(RecapConstants.CREATE_USER));
 
+    }
+
+    @RequestMapping(value="/roles",method= RequestMethod.POST)
+    @ApiOperation(value="roles authentication",notes="Used to Authorizer User for Roles",position = 0,consumes = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Role authentication success")})
+    public boolean roles(@RequestBody UsernamePasswordToken usernamePasswordToken) {
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(usernamePasswordToken);
+        List<Integer> roleId = userManagementService.getRolesForUser((Integer) subject.getPrincipal());
+        if(roleId.contains(1)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
