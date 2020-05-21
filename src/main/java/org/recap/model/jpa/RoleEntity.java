cce04c5ade335d1a151ae50b860f8fa0413a72fe
NSSuresh11 <nssuresh.subramanian@gmail.com>
2020-05-21 19:16:32 +0530

    Tech Upgrade Entity Class Changes

--- src/main/java/org/recap/model/jpa/RoleEntity.java
+++ src/main/java/org/recap/model/jpa/RoleEntity.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Table(name="roles_t",schema="recap",catalog="")
 public class RoleEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="role_id")
     private Integer roleId;
 
