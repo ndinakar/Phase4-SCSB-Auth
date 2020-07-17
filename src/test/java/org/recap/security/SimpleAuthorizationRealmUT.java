e6b8ab037c3aa519f5af2091b1b871000245b5b4
JancyRoachR <jancy.roach@htcindia.com>
2020-07-17 10:17:03 +0530

    Changes in UT class change##7

--- src/test/java/org/recap/security/SimpleAuthorizationRealmUT.java
+++ src/test/java/org/recap/security/SimpleAuthorizationRealmUT.java
@@ -2,22 +2,48 @@ package org.recap.security;
 
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.junit.Before;
 import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.recap.BaseTestCase;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
 import org.recap.security.realm.SimpleAuthorizationRealm;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
 
-public class SimpleAuthorizationRealmUT extends BaseTestCase {
-@Autowired
-SimpleAuthorizationRealm simpleAuthorizationRelam;
-    @Test
-    public void doGetAuthenticationInfo(){
+import static org.mockito.ArgumentMatchers.eq;
 
+
+//@RunWith(MockitoJUnitRunner.class)
+public class SimpleAuthorizationRealmUT{
+/*@InjectMocks
+SimpleAuthorizationRealm simpleAuthorizationRelam;*/
+
+   /* @Mock
+    private AuthenticationService authenticationService;
+
+    @Mock
+    private AuthorizationService authorizationService;
+
+    @Mock
+    AuthenticationInfo authenticationInfo;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+*/
+    /*@Test
+    public void doGetAuthenticationInfo( ) throws Exception {
         String loginUser="john:CUL";
         AuthenticationToken authenticationToken = null;
-        AuthenticationInfo authenticationInfo;
-       // authenticationInfo = simpleAuthorizationRelam.doGetAuthenticationInfo(authenticationToken);
-    }
+        String testMethod = "doGetAuthenticationInfo";
+        SimpleAuthorizationRealm mockObject = PowerMockito.mock(SimpleAuthorizationRealm.class);
+        PowerMockito.doReturn( authenticationInfo ).when( mockObject, testMethod, eq(authenticationToken) );
+
+    }*/
 }
