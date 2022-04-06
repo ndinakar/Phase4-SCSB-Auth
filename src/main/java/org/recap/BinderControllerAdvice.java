47ccf3fa756d5442f9ffb471dc0e7e3ae266566c
ndinakar <dnaramala@ciber.com>
2022-04-06 18:09:02 +0530

    0 vulnerability fix

---
+++ src/main/java/org/recap/BinderControllerAdvice.java
@@ -0,0 +1,21 @@
+package org.recap;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
+@Slf4j
+public class BinderControllerAdvice {
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
+        dataBinder.setDisallowedFields(denylist);
+    }
+
+}
